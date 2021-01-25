10c78b85daa31a9c5bbdb87d5d02b443901ce54e
Jrbdxa <leandre.nguyen-muller@isen.yncrea.fr>
2021-01-25 12:29:22 +0100

    View binding and redirection for Entrees button

--- app/src/main/java/fr/isen/nguyen/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/nguyen/androiderestaurant/HomeActivity.kt
@@ -2,6 +2,7 @@ package fr.isen.nguyen.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
 import fr.isen.nguyen.androiderestaurant.databinding.ActivityHomeBinding
 
 private lateinit var binding: ActivityHomeBinding
@@ -10,5 +11,17 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.entrees.setOnClickListener{
+            val intent = Intent(this, ListeEntreesActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
+
 }
---
+++ app/src/main/java/fr/isen/nguyen/androiderestaurant/ListeEntreesActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.nguyen.androiderestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListeEntreesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_liste_entrees)
+    }
+}
